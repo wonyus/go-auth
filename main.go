abdb85bf58124ebba1803b729e1aa1413e666c23
WonYus <lucy_oliv@outlook.com>
2023-05-25 08:13:26 +0700

    dot env

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"log"
 	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"github.com/wonyus/go-auth/controllers"
 	"github.com/wonyus/go-auth/initializers"
 	"github.com/wonyus/go-auth/middleware"
@@ -62,10 +64,14 @@ func getAlbumByID(c *gin.Context) {
 }
 
 func main() {
-	port := os.Getenv("PORT")
-	router := gin.Default()
 	// router.Use(middleware.Logger())
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+	port := os.Getenv("PORT")
 
+	router := gin.Default()
 	router.GET("/", func(c *gin.Context) {
 		c.JSON(http.StatusOK, gin.H{"message": "hello"})
 	})
