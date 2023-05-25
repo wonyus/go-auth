db2a6482bd67ea436c60da5aecb845c3b0196977
WonYus <lucy_oliv@outlook.com>
2023-05-25 09:50:38 +0700

    fix

--- initializers/loadEnvVariables.go
+++ initializers/loadEnvVariables.go
@@ -2,13 +2,14 @@ package initializers
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnvVariables() {
 	err := godotenv.Load()
-
+	os.Getenv("PORT")
 	if err != nil {
 		log.Fatal("Error Loading .env file")
 	}
